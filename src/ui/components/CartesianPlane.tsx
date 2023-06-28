1cdfc46a27f792cd6cfd446391ee53019d70f1c1
omarc <omarcalderon1912@gmail.com>
2023-06-28 15:59:25 -0400

    canvas hell...

--- src/ui/components/CartesianPlane.tsx
+++ src/ui/components/CartesianPlane.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import VariableProps from "../props/VariableProps";
-import Graph from "./children/GraphComponent";
+import GraphComponent from "./children/GraphComponent";
 import { MathComponent } from "mathjax-react";
 import styles from "../../css/components/CartesianPlane.module.css";
 
 const CartesianPlane: React.FC<VariableProps> = (props) => {
     return(
         <div className={styles.planeFormatting}>
-            <Graph
+            <GraphComponent
                 verticalAxisLabel={
                     <MathComponent tex="P_i"/>
                 }
--- src/ui/components/children/GraphComponent.tsx
+++ src/ui/components/children/GraphComponent.tsx
@@ -1,24 +1,49 @@
+import { useEffect, FC, createRef } from 'react';
 import styles from '../../../css/children/GraphComponent.module.css';
+import React from 'react';
 
 interface GraphProps {
-    // verticalAxisStep: number
     verticalAxisLabel: JSX.Element
     horizontalAxisLabel: JSX.Element
+    // verticalAxisStep: number
+    // horizontalAxisStep: number
+    // maxY: number
+    // minY: number
+    // maxX: number
+    // minX: number
 }
  
-const Graph: React.FC<GraphProps> = (props) => {
+const GraphComponent: FC<GraphProps> = (props) => {
     return ( 
         <div className={styles.graphComponentContainer}>
             <div className={styles.verticalAxisFormatting}>
+                <div className={styles.verticalAxisLabel}>
+
+                </div>
+                <canvas
+                    id="vertical-canvas"
+                    className={styles.verticalAxisMarkings}
+                    width={20}
+                    height={100}
+                >
+                </canvas>
             </div>
+
             <div className={styles.graphContainer}>
 
             </div>
 
             <div className={styles.horizontalAxisFormatting}>
+                <div className={styles.horizontalAxisLabel}>
+                
+                </div>
             </div>
         </div>
      );
 }
  
-export default Graph;
+export default GraphComponent;
+function useState(arg0: CanvasRenderingContext2D | null): [any, any] {
+    throw new Error('Function not implemented.');
+}
+
